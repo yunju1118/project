32d107adad1ff0a4b44c3ffb55f74d1af8d5f7e4
Tata <annie11180913@gmail.com>
2023-09-26 21:54:26 +0800

    add some new

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("hi");
+const http = require("http");
+const url = require("url");
+// console.log(http);
+
+const server = http.createServer((req, res) => {
+  let parsedURL = url.parse(req.url);
+  console.log(parsedURL);
+  console.log(parsedURL.pathname);
+  //   console.log(req.url);
+  res.write("Hello, " + parsedURL.pathname);
+  res.end();
+});
+// console.log(server);
+
+server.listen(3501, () => {
+  console.log("Server is running on port 3501.");
+});
